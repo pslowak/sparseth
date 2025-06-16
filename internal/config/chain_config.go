2275083e96a8c568aac971c14914b9d3e1b5a1e8
Philipp Slowak <philipp.slowak@tuwien.ac.at>
2025-06-16 14:56:16 +0200

    feat: add chain config for Anvil

---
+++ internal/config/chain_config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"github.com/ethereum/go-ethereum/params"
+	"math/big"
+)
+
+var (
+	DefaultCancunBlobConfig = &params.BlobConfig{
+		Target:         3,
+		Max:            6,
+		UpdateFraction: 3338477,
+	}
+
+	DefaultPragueBlobConfig = &params.BlobConfig{
+		Target:         6,
+		Max:            9,
+		UpdateFraction: 5007716,
+	}
+
+	// AnvilChainConfig is the parameters to
+	// run a node on the Anvil network.
+	AnvilChainConfig = &params.ChainConfig{
+		ChainID:                 big.NewInt(31337),
+		HomesteadBlock:          big.NewInt(0),
+		DAOForkBlock:            nil,
+		DAOForkSupport:          false,
+		EIP150Block:             big.NewInt(0),
+		EIP155Block:             big.NewInt(0),
+		EIP158Block:             big.NewInt(0),
+		ByzantiumBlock:          big.NewInt(0),
+		ConstantinopleBlock:     big.NewInt(0),
+		PetersburgBlock:         big.NewInt(0),
+		IstanbulBlock:           big.NewInt(0),
+		MuirGlacierBlock:        big.NewInt(0),
+		BerlinBlock:             big.NewInt(0),
+		LondonBlock:             big.NewInt(0),
+		ArrowGlacierBlock:       big.NewInt(0),
+		GrayGlacierBlock:        big.NewInt(0),
+		MergeNetsplitBlock:      big.NewInt(0),
+		ShanghaiTime:            newUint64(0),
+		CancunTime:              newUint64(0),
+		PragueTime:              newUint64(0),
+		OsakaTime:               nil,
+		VerkleTime:              nil,
+		TerminalTotalDifficulty: big.NewInt(0),
+		Ethash:                  new(params.EthashConfig),
+		Clique:                  nil,
+		BlobScheduleConfig: &params.BlobScheduleConfig{
+			Cancun: DefaultCancunBlobConfig,
+			Prague: DefaultPragueBlobConfig,
+		},
+	}
+)
+
+func newUint64(val uint64) *uint64 {
+	return &val
+}
