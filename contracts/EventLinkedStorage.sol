0fcc573c7e0f6e15e5f633450ef15d0a23f3497c
Philipp Slowak <philipp.slowak@tuwien.ac.at>
2025-06-30 20:15:50 +0200

    feat: add interaction counter to smart contract

--- contracts/EventLinkedStorage.sol
+++ contracts/EventLinkedStorage.sol
@@ -19,6 +19,24 @@ contract EventLinkedStorage {
     /// the new head.
     bytes32 private head;
 
+    /// @notice A monotonic counter tracking external writes.
+    /// @dev This counter is incremented every time a publicly
+    /// accessible function is called that modifies state. It
+    /// is used by the sparse node to verify the completeness
+    /// of received transaction data involving this contract.
+    ///
+    /// In the context of Solidity, the counter must be
+    /// incremented in functions that:
+    /// - Are marked as `external` or `public`
+    /// - Modify the state of the contract
+    ///
+    /// Functions that are marked `view` or `pure`, or those
+    /// with `internal` or `private` visibility do not require
+    /// the counter to be incremented, as they either do not
+    /// modify state or cannot be called from outside the
+    /// contract.
+    uint256 private counter;
+
     /// @notice The most recently stored value.
     uint256 private value;
 
@@ -27,6 +45,7 @@ contract EventLinkedStorage {
      * @param val - uint256 to be stored
      */
     function store(uint256 val) external {
+        counter++;
         head = keccak256(abi.encode(head, msg.sender, val));
         value = val;
 
